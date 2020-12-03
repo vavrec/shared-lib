2270afaafa2c6c532f13ed7bfce9c3228df55161
Vavrečka David <david.vavrecka@gmail.com>
2020-12-03 12:39:48 +0100

    aaa

--- vars/ok.groovy
+++ vars/ok.groovy
@@ -3,9 +3,7 @@ import cz.vavrecka.Variables
 def call() {
     echo  Variables.SLACK_MESSAGE;
      println( Variables.testMap.size());
-    echo "konec";
     Variables.testMap.put("age","ssss");
      println( Variables.testMap.size());
-     test2();
-     println( Variables.testMap.size());
+      echo "konec";
 }
--- vars/test2.groovy
+++ vars/test2.groovy
@@ -4,4 +4,5 @@ def call() {
     //Variables.variables["name"] == "david"
     Variables.testMap.put("test2","ssss");
     echo "test2";
+    println( Variables.testMap.size());
 }

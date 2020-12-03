d7b16318e764ecbe8bf1a236ae706c9f1668296d
Vavrečka David <david.vavrecka@gmail.com>
2020-12-03 12:31:49 +0100

    aaa

--- src/cz/vavrecka/Variables.groovy
+++ src/cz/vavrecka/Variables.groovy
@@ -2,7 +2,8 @@ package cz.vavrecka
 
 class Variables {
 
-    static final String SLACK_MESSAGE = "Sending Slack Notification";
+    static final String SLACK_MESSAGE = 'Sending Slack Notification';
+    public static testMap = [name : 'david'] ;
     //public static Map<String,String> variables = [:] ;
 
 }
--- vars/ok.groovy
+++ vars/ok.groovy
@@ -1,6 +1,9 @@
 import cz.vavrecka.Variables
 
 def call() {
-   echo  Variables.SLACK_MESSAGE
-   echo "konec"
+    echo  Variables.SLACK_MESSAGE;
+    echo  Variables.testMap.size();
+    echo "konec";
+    Variables.testMap.put("age","ssss");
+    echo  Variables.testMap.size();
 }

ee858ce6b4c05eb3f621de30765f668254c7e772
Vavrečka David <david.vavrecka@gmail.com>
2020-12-05 09:47:38 +0100

    ok

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -10,8 +10,8 @@ def call(String version) {
         stage("deploy") {
             echo "ola la"
             sshagent(['TEST-SSH']) {
-               def connection = connectionData['user'] +'@'+connectionData['address']+':' + connectionData['path']
-  //              sh 'pwd'
+                def connection = connectionData['user'].toString() + '@' + connectionData['address'].toString() + ':' + connectionData['path'].toString()
+                //              sh 'pwd'
 //                sh "unzip colman-${version}.zip"
 //                sh 'scp -r ./colman-$' + version + '/batch ' + connection
 //                sh """rm -rf colman-*"""
@@ -21,6 +21,6 @@ def call(String version) {
 }
 
 @NonCPS
-private  Object parseSetUp(){
+private Object parseSetUp() {
     return new JsonSlurper().parseText(libraryResource('prod.json'))
 }
